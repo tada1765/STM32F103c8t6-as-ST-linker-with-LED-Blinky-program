89095d19a5d25a25cc2c37631dc2cf2e67fb72df
tada1765 <www.tada1765@gmail.com>
2019-09-16 16:19:18 +0800

    added c.file and h.file.

---
+++ src/project_name.h
@@ -0,0 +1,5 @@
+#ifndef _PROJECT_NAME_H
+#define _PROJECT_NAME_H
+
+
+#endif // _PROJECT_NAME_H
---
+++ test/test_project_name.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "project_name.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_project_name_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement project_name");
+}
